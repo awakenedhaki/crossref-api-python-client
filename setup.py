013d1e4865b55cbd4766fcd6977aec94ab3e0407
awakenedhaki <rodrigo.vallejosch@gmail.com>
2020-04-09 16:12:29 -0700

    Importing version, updating name and author

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 import setuptools
 
+from crossrefapiclient import __version__
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="awakenedhaki",
-    version="0.1.0",
-    author="Rodrigo Vallejos",
+    name="crossref-api-client",
+    version=__version__,
+    author="awakenedhaki",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
